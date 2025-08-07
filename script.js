83dae1e8b1c8cac1f09f5425809ad52b45e1697f
shalati mabobo <shalatimabobo@gmail.com>
2025-08-07 14:08:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+/**creating a button click show hide navbar */
+var togglebtn = document.querySelector('.togglebtn');
+var nav = document.querySelector('.navlinks');
+var links = document.querySelectorAll('.navlinks li');
+togglebtn.addEventListener('click', function() {
+    this.classList.toggle('click');
+    nav.classList.toggle('open');
+    });
+
+    var typed=new Typed('.input',{
+        strings:['  Software Developer',  'Web Developer', '  Multimedia Specialist' , 'Graphic Designer'],
+        typedSpeed: 70,
+        backSpeed: 55,
+        loop:true,
+    })
+
+    function toggleText() {
+        const moreText = document.getElementById("moreText");
+        const btn= document.getElementById("readMoreBtn");
+        if (moreText.style.display === "none") {
+            moreText.style.display = "block";
+            btn.textContent = "Read Less";
+        } else {
+            moreText.style.display = "none";
+            btn.textContent = "Read More";
+        }
+    }
+    document.getElementById('contact-form').addEventListener('submit',function(e){
+ e.preventDefault();
+ alert("Thank you for reaching out!  I'll get back to you soon." );
+ this.reset();
+    });
+
+
+
+//     (function(){
+//         emailjs.init("KbnotOfVXjdPIqWFD");
+//     })();
+//     document.getElementById('contact-form').addEventListener('submit', function(event){
+//  event.preventDefault();
+//  emailjs.sendForm('service_5xysxol', 'template_mkik6ks',this)
+//     .then(()=> {
+//         alert('Your message has been sent successfully!');
+//         this.reset();
+//     }, (error) => {
+//         alert('Failed to send your message. Please try again later.\n'+JSON.stringify(error));
+//     });
+//     });
+
+
+
+(function () {
+    emailjs.init("KbnotOfVXjdPIqWFD"); // Your EmailJS user/public key
+})();
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const templateParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        
+    };
+
+    emailjs.send('service_5xysxol', 'template_mkik6ks', templateParams)
+        .then(() => {
+            alert('Your message has been sent successfully!');
+            document.getElementById('contact-form').reset(); // Reset the form
+        }, (error) => {
+            alert('Failed to send your message. Please try again later.\n' + JSON.stringify(error));
+        });
+});
+
+
